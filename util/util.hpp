0a124bc28df4a3c8c824c77a2f14983bb2fe3f53
Sahil Kale <sahil.kale1@gmail.com>
2023-10-21 17:47:27 +0200

    [Linter Addin] Add in Cppcheck linter and fix bugs

--- util/util.hpp
+++ util/util.hpp
@@ -1,7 +1,7 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
-// Define an unused macro to silence compiler warnings
-#define UNUSED(x) (void)(x)
+template <class T>
+void IGNORE(const T&) {}
 
 #endif  // UTIL_HPP
